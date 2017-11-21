f62dbf2e1779c68a8701dc74f5e6019cff1dd33b
Paul Barajas <paul.barajas@linux.com>
2017-11-20 19:04:36 -0600

    Divide & Conquer algorithm with recursive case

--- binary_search.py
+++ binary_search.py
@@ -2,8 +2,10 @@
 """simple binary search"""
 
 def binary_search(my_list, item):
+    """basic algorithm for binary search"""
     low = 0
     high = len(my_list) - 1
+
     while low <= high:
         middle = (low + high) // 2
         guess = my_list[middle]
@@ -11,11 +13,26 @@ def binary_search(my_list, item):
             return middle
         if guess > item:
             high = middle - 1
-        else:
-            low = middle + 1
+        low = middle + 1
     return None
 
-def recursive_binary_search()
+def recursive_binary_search(my_list, item):
+    """Divide & Conquer algorithm with recursive case for
+    binary search"""
+    if not my_list:
+        return 0
+
+    middle = len(my_list) // 2
+    guess = my_list[middle]
+
+    if item == guess:
+        return middle
+    if guess > item:
+        return recursive_binary_search(my_list[:middle], item)
+    return recursive_binary_search(my_list[middle:], item)
+
 
 LIST = [1, 3, 5, 7, 9, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
-print(binary_search(LIST, 5))
+
+print(binary_search(LIST, 1597))
+print(recursive_binary_search(LIST, 1441))
